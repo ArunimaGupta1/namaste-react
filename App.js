19b85fd8b7e6d14eb1f85ff13647bf5aba5c8154
ArunimaGupta1 <arunimagupta71@gmail.com>
2024-02-28 16:17:18 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,25 @@
+// 
+// <div id="parent">
+//    <div id="child">
+//        <h1>I am h1 tag</h1>
+//        <h2>I am h2 tag</h2>
+//    </div>
+// </div>
+// 
+// 
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div",
+        { id: "child" },
+        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]),
+    React.createElement("div",
+        { id: "child2" },
+        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")])
+]) //array of children
+
+//JSX
+const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!");
+// console.log(heading)  //object
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
